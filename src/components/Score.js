25b96ed937cb9f63a97a9c564467cd5afe66cf76
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-26 16:51:15 +0300

    Create Score

---
+++ src/components/Score.js
@@ -0,0 +1,14 @@
+import PropTypes from "prop-types";
+import styles from "styled-components";
+
+const Wrapper = styles.div`
+  margin-right: auto;
+`;
+
+const Score = (props) => {
+  return <Wrapper>Score</Wrapper>;
+};
+
+Score.propTypes = {};
+
+export default Score;
