bf3ede1f1ddf4595a9b1cf1c522cb9a0bd1b2e8c
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-26 17:01:57 +0300

    Show scores

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,12 +3,22 @@ import styles from "styled-components";
 
 const Wrapper = styles.div`
   margin-right: auto;
+  font-size: 1.5rem;
 `;
 
-const Score = (props) => {
-  return <Wrapper>Score</Wrapper>;
+const Score = ({ score: { bestScore, currentScore } }) => {
+  return (
+    <Wrapper>
+      Current Score: {currentScore} | Best Score: {bestScore}
+    </Wrapper>
+  );
 };
 
-Score.propTypes = {};
+Score.propTypes = {
+  score: PropTypes.shape({
+    bestScore: PropTypes.number.isRequired,
+    currentScore: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default Score;
