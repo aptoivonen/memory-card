7aecafd9ee6a9abaa7685567deb3ae6160383943
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-26 19:41:40 +0300

    Tweak styles

--- src/components/Card.styles.js
+++ src/components/Card.styles.js
@@ -3,15 +3,20 @@ import styled from "styled-components";
 export const Container = styled.article`
   display: flex;
   flex-direction: column;
-  width: 200px;
-  height: 300px;
-  padding: 1rem;
+  align-items: center;
+  justify-content: space-between;
+  width: 180px;
+  height: 250px;
+  padding: 2.5rem;
   background: greenyellow;
   border: 1px solid black;
 `;
 
 export const Name = styled.h2`
-  font-size: 2rem;
+  order: 2;
+  font-size: 1.5rem;
+  letter-spacing: 0.3ch;
+  color: #333;
 `;
 
 export const Image = styled.img`
