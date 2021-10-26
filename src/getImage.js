eec0f876dd66d0ac283a78c8de8a1a2720eb93a5
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-26 19:34:01 +0300

    Create image getter

---
+++ src/getImage.js
@@ -0,0 +1,16 @@
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((item) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(
+  require.context("../public/images", false, /\.(png|jpe?g|svg)$/)
+);
+
+export default function getImage(imageFileName) {
+  console.log(images[imageFileName]);
+  return images[imageFileName].default;
+}
