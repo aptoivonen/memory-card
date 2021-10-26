9f0c126948b1cf4697917bcd902210e63840a0cb
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-26 17:01:40 +0300

    Add state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
+import { useState } from "react";
 import cards from "./cards";
 import GlobalStyle from "./components/GlobalStyle";
 import { Wrapper, Header, MainTitle, Main, Footer } from "./App.styles";
 import Score from "./components/Score";
 
 function App() {
+  const [state, setState] = useState({
+    bestScore: 0,
+    currentScore: 0,
+    currentLevel: 1,
+    selectedCards: [],
+    currentLevelCards: null,
+  });
+
+  const score = {
+    bestScore: state.bestScore,
+    currentScore: state.currentScore,
+  };
+
   return (
     <Wrapper>
       <GlobalStyle />
       <Header>
         <MainTitle>Memory Game</MainTitle>
-        <Score />
+        <Score score={score} />
       </Header>
       <Main></Main>
       <Footer>
         <div>
-          Icons made by
+          Icons made by{" "}
           <a
             href="https://www.flaticon.com/authors/smashicons"
             title="Smashicons"
           >
             Smashicons
-          </a>
-          from
+          </a>{" "}
+          from{" "}
           <a href="https://www.flaticon.com/" title="Flaticon">
             www.flaticon.com
           </a>
