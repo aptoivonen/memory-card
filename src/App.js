8c52caf443a9d8016144c886e7656acecff93fa3
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-26 16:48:09 +0300

    Refactor styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import cards from "./cards";
 import GlobalStyle from "./components/GlobalStyle";
+import { Wrapper, Header, MainTitle, Main, Footer } from "./App.styles";
+import Score from "./components/Score";
 
 function App() {
   return (
-    <div>
+    <Wrapper>
       <GlobalStyle />
-      <div>
-        Icons made by
-        <a
-          href="https://www.flaticon.com/authors/smashicons"
-          title="Smashicons"
-        >
-          Smashicons
-        </a>
-        from
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
-    </div>
+      <Header>
+        <MainTitle>Memory Game</MainTitle>
+        <Score />
+      </Header>
+      <Main></Main>
+      <Footer>
+        <div>
+          Icons made by
+          <a
+            href="https://www.flaticon.com/authors/smashicons"
+            title="Smashicons"
+          >
+            Smashicons
+          </a>
+          from
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </Footer>
+    </Wrapper>
   );
 }
 
---
+++ src/App.styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 100px;
+  padding: 1rem;
+  background: #ccc;
+`;
+
+export const MainTitle = styled.h1`
+  margin-right: auto;
+  font-size: 1.5rem;
+`;
+
+export const Main = styled.main`
+  flex: 1 0;
+  background: #ddd;
+`;
+
+export const Footer = styled.footer`
+  height: 100px;
+  padding: 1rem;
+  background: #aaa;
+`;
