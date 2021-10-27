b01b0b9d91066938f26d7a6fa9a30cac1f9f1f10
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-27 15:45:55 +0300

    Change Card look

--- src/components/Card.styles.js
+++ src/components/Card.styles.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.article`
+  --base-size: 230px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 180px;
-  height: 250px;
+  width: calc(2 / 3 * var(--base-size));
+  height: var(--base-size);
   margin: 0 2rem 2rem 0;
-  padding: 1rem 0;
+  /* padding: 1rem 0; */
   background: greenyellow;
   transition: transform 0.07s ease-in;
   cursor: pointer;
@@ -20,8 +21,12 @@ export const Container = styled.article`
 `;
 
 export const Name = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
   order: 2;
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   text-align: center;
   letter-spacing: 0.3ch;
   color: #333;
