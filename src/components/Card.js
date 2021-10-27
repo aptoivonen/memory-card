2f955fcf3c17a26be1e934cef19ab851ec3c1a5b
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-27 19:28:43 +0300

    Implement Card selecting

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,28 @@
 import PropTypes from "prop-types";
+import { useRef } from "react";
 import { Container, Name, Image } from "./Card.styles";
 
 const Card = ({ card: { id, name, url }, selectCard }) => {
+  const ref = useRef();
+
+  const handleClick = () => {
+    ref.current.blur();
+    selectCard(id);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.code === "Space") {
+      handleClick();
+    }
+  };
+
   return (
-    <Container tabIndex={0} onClick={() => selectCard(id)}>
+    <Container
+      tabIndex={0}
+      ref={ref}
+      onClick={handleClick}
+      onKeyPress={handleKeyPress}
+    >
       <Name>{name}</Name>
       <Image src={url} />
     </Container>
