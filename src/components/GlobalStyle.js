7f2c4d7e3e92994f721a53a8a8ed80ce06a280ce
Aleksi Toivonen <aptoivonen@mail.com>
2021-10-28 20:42:13 +0300

    Move font import to index.html
    This is styled-components' preference

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap');
-
   *,
   *::before,
   *:after {
